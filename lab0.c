81727a76d592fde6d184a7c029db62f0001d1d13
Ballardo Calderon <ballardo.calderon.764@my.csun.edu>
2020-02-08 16:55:51 -0800

    Second commit This one fixes errors shown when using Wall and Wextra

--- lab0.c
+++ lab0.c
@@ -1,1 +1,55 @@
-nclude<stdio.h>                                                                                                                         #include<sys/types.h>                                                                                                                     #include<sys/stat.h>                                                                                                                      #include<fcntl.h>                                                                                                                         #include<string.h>                                                                                                                        #include<stdlib.h>                                                                                                                        int main(int argc, char *argv[])                                                                                                          {                                                                                                                                                 if(argc <= 1)                                                                                                                             {                                                                                                                                                 printf("Error: No Input\n");                                                                                                              exit(0);                                                                                                                          }                                                                                                                                         if((open(argv[1], O_RDONLY)) == -1)                                                                                                       {                                                                                                                                                 int cmp = strcmp(argv[1], "-");                                                                                                           int x = 1;                                                                                                                                if(cmp == 0) x = 2;                                                                                                                       char str[9];                                                                                                                              char str2[8];                                                                                                                             for(x; x < argc; x++)                                                                                                                     {                                                                                                                                                 strcpy(str,argv[x]);                                                                                                                      int sum = 0;                                                                                                                              unsigned len = (unsigned)strlen(argv[x]);                                                                                                 if(len < 9)                                                                                                                               {                                                                                                                                                 for(int y = len; y < 9; y++)                                                                                                              {                                                                                                                                                 str[y] = '0';                                                                                                                     }                                                                                                                                         str[8] = '\0';                                                                                                                    }                                                                                                                                         for(int x = 0; x < 8; x++)                                                                                                                {                                                                                                                                                 str2[x] = str[x+1];                                                                                                                       sum += (int)str[x];                                                                                                               }                                                                                                                                         //char c = strtol(str2,0,2);                                                                                                              char *ptr;                                                                                                                                char c = strtol(str2,&ptr,2);                                                                                                             int a = (int)c;                                                                                                                                                                                                                                                                     printf("%s\t%c\t%i\t",str,c,a);                                                                                                           if(sum % 2 == 0)                                                                                                                          {                                                                                                                                                 printf("EVEN\n");                                                                                                                 }                                                                                                                                         else                                                                                                                                      {                                                                                                                                                 printf("ODD\n");                                                                                                                  }                                                                                                                                 }                                                                                                                                 }                                                                                                                                 }             
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+        if(argc <= 1)
+        {
+                printf("Error: No Input\n");
+                exit(0);
+        }
+        if((open(argv[1], O_RDONLY)) == -1)
+        {
+                int cmp = strcmp(argv[1], "-");
+                int x = 1;
+                if(cmp == 0) x = 2;
+                char str[9];
+                char str2[8];
+                for(int y = x; y < argc; x++)
+                {
+                        strcpy(str,argv[y]);
+                        int sum = 0;
+                        unsigned len = (unsigned)strlen(argv[y]);
+                        if(len < 9)
+                        {
+                                for(int z = len; z < 9; z++)
+                                {
+                                        str[z] = '0';
+                                }
+                                str[8] = '\0';
+                        }
+                        for(int z = 0; z < 8; z++)
+                        {
+                                str2[z] = str[z+1];
+                                sum += (int)str[z];
+                        }
+                        //char c = strtol(str2,0,2);
+                        char *ptr;
+                        char c = strtol(str2,&ptr,2);
+                        int a = (int)c;
+
+                        printf("%s\t%c\t%i\t",str,c,a);
+                        if(sum % 2 == 0)
+                        {
+                                printf("EVEN\n");
+                        }
+                        else
+                        {
+                                printf("ODD\n");
+                        }
+                }
+        }
+}
