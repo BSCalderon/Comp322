be51e78a12e3bbdf0ea062790c7258374273907e
Ballardo Calderon <ballardo.calderon.764@my.csun.edu>
2020-03-23 19:30:07 -0700

    Update updating pretty much everything.

--- Lab2/tube.c
+++ Lab2/tube.c
@@ -8,6 +8,23 @@
 #define READ 0
 #define WRITE 1
 
+void separateArgv(char *argv[], char *newArgv1[], char *newArgv2[], int argv1, int argc)
+{
+	
+	int a = 1;
+	for(int x = 0; x < argv1-1; x++)
+	{
+		newArgv1[x] = argv[a++];
+	}
+	newArgv1[argv1-1] = NULL;
+	for(int x = 0; x < argc-argv1-1; x++)
+	{
+		
+		newArgv2[x] = argv[++a];
+	}
+	newArgv2[argc-argv1-1] = NULL;
+}
+
 int main(int argc, char *argv[])
 {
 	pid_t child1;
@@ -28,6 +45,22 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	
+	while(strcmp(argv[argv1],",")!=0)
+	{
+		argv1++;
+	}
+	char *newArgv1[argv1];
+	char *en[] = {NULL};
+	int argv2 = argc-argv1-1;
+	char *newArgv2[argv2];
+	char *envp[] = {NULL};
+	separateArgv(argv,newArgv1, newArgv2, argv1, argc);
+	
+	
+	
+	
+	
 	child1 = fork();
 	if(child1 < 0)
 	{
@@ -37,7 +70,7 @@ int main(int argc, char *argv[])
 	else if (child1 == 0)
 	{
 		//child1
-		//printf("Child1: pid:%d, ppid:%d \n", getpid(), getppid());
+		
 		if(close(fd[WRITE]) == -1)
 		{
 			fprintf(stderr, "Error closing reading end of pipe.\n");
@@ -45,28 +78,19 @@ int main(int argc, char *argv[])
 		}
 		dup2(fd[READ],STDIN_FILENO);
 		
-		//tube /bin/cat filename , /usr/bin/head -2
-		//7 argcs
+	
 		
-		while(strcmp(argv[argv1],",")!=0)
-		{
-			argv1++;
-		}
-		char *newArgv1[argv1];
-		for(int x = 0; x < argv1-1; x++)
-		{
-			newArgv1[x] = argv[x+1];
-		}
-		newArgv1[argv1-1] = NULL;
-		char *en[] = {NULL};
-		for(int x = argv1+1; x < argc; x++)
-		{
-			write(fd[WRITE],argv[x],strlen(argv[x])+1);
-		}
+		
+		
+		read(fd[READ],newArgv1,sizeof(newArgv1)+1);
 		close(fd[WRITE]);
 		execve(newArgv1[0], newArgv1, en);
-		fprintf(stderr, "Error in execve.\n");
-		exit(EXIT_FAILURE);
+		// if(execve(newArgv1[0], newArgv1, en) == -1)
+		// {
+		// 	fprintf(stderr, "Error in execve for child 1.\n");
+		// 	exit(EXIT_FAILURE);
+		// }
+		
 	}
 	else
 	{
@@ -81,25 +105,26 @@ int main(int argc, char *argv[])
 		{
 			//child2
 			//printf("Child2: pid:%d, ppid:%d \n", getpid(), getppid());
-			dup2(fd[READ],STDOUT_FILENO);
+			dup2(fd[READ],STDIN_FILENO);
 			if(close(fd[WRITE]) == -1)
 			{
 				fprintf(stderr, "Error closing writing end of pipe.\n");
 				exit(EXIT_FAILURE);
 			}
-			char *newArgv2[argc - argv1 -1];
-			for(int x = argv1+1; x < argc-1; x++)
-			{
-				read(fd[READ],newArgv2[x],strlen(newArgv2[x])+1);
-			}
-			char *envp[] = {NULL};
-			execve(newArgv2[0],newArgv2,envp);
-
+			read(fd[READ], newArgv2,sizeof(newArgv2)+1);
+			execve(newArgv2[0], newArgv2, envp);
+			//if(execve(newArgv2[0], newArgv2, envp) == -1)
+			// {
+			// 	fprintf(stderr, "Error in execve for child 2.\n");
+			// 	exit(EXIT_FAILURE);
+			// }
 		}
 		else
 		{
 			//parent
-			fprintf(stderr,"Parent: child1 pid:%d, child2 ppid:%d \n", child1, child2);
+			
+			fprintf(stderr, "%s: $$ = %d\n", newArgv1[0], child1);
+			fprintf(stderr, "%s: $$ = %d\n", newArgv2[0], child2);
 			close(fd[READ]);
 
 			write(fd[WRITE],newArgv1,sizeof(newArgv1));
@@ -112,7 +137,7 @@ int main(int argc, char *argv[])
 			waitpid(child1, &status1, 0);
 			waitpid(child2, &status2, 0);
 
-			fprintf(stderr, "%s: $? = %d\n",newArgv1[0], status1);
+			fprintf(stderr, "\n%s: $? = %d\n",newArgv1[0], status1);
 			fprintf(stderr, "%s: $? = %d\n",newArgv2[0], status2);
 		}
 	}
